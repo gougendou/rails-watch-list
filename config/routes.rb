3451e854242e1aaa9974b42615b55a635a63f92d
SamTreon <stat123@hotmail.co.uk>
2023-02-23 14:14:02 +0000

    added all the BE routing, controllers and views for lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
+  # get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists, only: %i(index show new create) do
+    resources :bookmarks, only: [:create]
+  end
+
 end
+
+# Features
+# Once again, you must have a precise idea of the features of your app in order to build your routes. Here is the list of features:
+
+# A user can see all the lists
+# GET "lists"
+# A user can see the details of a given list and its name
+# GET "lists/42"
+# A user can create a new list
+# GET "lists/new"
+# POST "lists"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "open-uri"
+require "json"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+movies=movies["results"]
+puts movies
+
+puts "creating movies..."
+movies.each do |movie|
+  new_movie = Movie.new
+  puts "added new movie"
+  new_movie.title = movie['original_title']
+  puts "added movie title"
+  new_movie.overview = movie['overview']
+  puts "added movie overview"
+  new_movie.poster_url = "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}"
+  puts "added movie url"
+  new_movie.rating = movie["vote_average"]
+  puts "added movie_rating"
+  new_movie.save
+end
+
+puts "finished creating movies..."
+
+
+
+# each movie has:
+# t.string "title" movie["original_title"]
+# t.string "overview" movie["overview"]
+# t.text "poster_url" https://image.tmdb.org/t/p/w500movie["poster_path"]
+# t.decimal "rating" movie["vote_average"]
